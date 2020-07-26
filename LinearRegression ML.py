f63bb8b04f3a9b0ccf0dc65329f25c8e3f538e75
Durgesh Vishwakarma <D4Techs@gmail.com>
2020-07-27 00:19:47 +0530

    Update and rename LinearRegression ML to LinearRegression ML.py

--- LinearRegression ML.py
+++ LinearRegression ML.py
@@ -1,5 +1,11 @@
+import pandas as pd
+import numpy as np
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
+
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
 lr = LinearRegression()
 lr.fit(x_train , y_train)
