e86141fe459833e68d87b4a12b23fa4b815a199e
Durgesh Vishwakarma <D4Techs@gmail.com>
2020-07-27 17:05:05 +0530

    Update LinearRegression ML.py

--- LinearRegression ML.py
+++ LinearRegression ML.py
@@ -4,15 +4,21 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+"""
+LR: build regression model
+train_test_split: split training & test data in dataset
+mean_squared_error: find error in model
+"""
 
 iris = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
+iris.columns = ["sepal.length", "sepal.width", "petal.length", "petal.width", "species"]
 
-x = iris.iloc[0]
-y = iris.iloc[1]
+x = iris[["sepal.length"]]
+y = iris[["sepal.width"]]
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
 lr = LinearRegression()
 lr.fit(x_train , y_train)
 y_predict = lr.predict(y_test)
 
-print( mean_squared_error(y_test, y_predict) )
+print("Error in prediction: " + str( mean_squared_error(y_test, y_predict)  ) )
