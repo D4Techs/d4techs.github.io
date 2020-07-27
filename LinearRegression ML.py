a8711a1f9ea442b65e3070d1a3b87e0867448997
Durgesh Vishwakarma <D4Techs@gmail.com>
2020-07-27 18:32:31 +0530

    Update LinearRegression ML.py

--- LinearRegression ML.py
+++ LinearRegression ML.py
@@ -8,17 +8,32 @@ from sklearn.metrics import mean_squared_error
 LR: build regression model
 train_test_split: split training & test data in dataset
 mean_squared_error: find error in model
+
+COLs in dataset: n1,n2,n1_sq,n1_cube,2*n1,3*n1,2*n1+5,n1*n2+10
+@save below as "regTest.csv "
+n1,n2,n1_sq,n1_cube,2*n1,3*n1,2*n1+5,n1*n2+10
+1,1,1,1,2,3,7,11
+2,6,4,8,4,6,9,22
+3,6,9,27,6,9,11,27
+4,4,16,64,8,12,13,26
+5,1,25,125,10,15,15,13
 """
+dataset = pd.read_csv("regTest.csv")
 
-iris = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
-iris.columns = ["sepal.length", "sepal.width", "petal.length", "petal.width", "species"]
+dataset.head()
+x = dataset[["n1"]]
+y = dataset[["2*n1+5"]]
 
-x = iris[["sepal.length"]]
-y = iris[["sepal.width"]]
-
+#split train & test data
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
+
+#create LinerReg model;train; predict
 lr = LinearRegression()
 lr.fit(x_train , y_train)
-y_predict = lr.predict(y_test)
+y_predict = lr.predict(x_test)
 
-print("Error in prediction: " + str( mean_squared_error(y_test, y_predict)  ) )
+#Calc error in model [test_data - predicted_data
+print("Error : " + str( mean_squared_error(y_test, y_predict) ) )
+
+#predict output for given input
+lr.predict(8.31)
